6b6be92ffbe98230c50cfe516ed751ad6b197279
LeoGiuseppe <leo.giuseppe@iisgalvanimi.edu.it>
2026-04-24 08:58:17 +0000

    esercizio2

--- animazione.js
+++ animazione.js
@@ -1,55 +1,105 @@
 function startGame() {
     myGameArea.start();
-    myGameArea.draw(redSquare);
+   animatedObject.loadImages();
 }
 
-var redSquare = {
-  width: 20,
-  height: 20,
-  x: 10,
-  y: 120,
-  color: "red"
-};
-var myGameArea = {
+var myGameArea = {  
     canvas : document.createElement("canvas"),
-     draw: function(component) {
-    this.context.fillStyle = component.color;
-    this.context.fillRect(component.x, component.y, component.width, component.height);
-  },
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
          this.interval = setInterval(updateGameArea, 20);
-    },
-    clear: function () {
+      },
+     draw: function(component) {
+    this.context.fillStyle = component.color;
+    this.context.fillRect(component.x, component.y, component.width, component.height);
+  },
+  clear: function () {
     this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+  },
+   drawGameObject: function(gameObject) {
+    this.context.drawImage(
+      gameObject.image,
+      gameObject.x,
+      gameObject.y,
+      gameObject.width,
+      gameObject.height
+    );
+  }
 }
-};
 
 function updateGameArea() {
      myGameArea.clear();
-  myGameArea.draw(redSquare);
-
+    myGameArea.drawGameObject(animatedObject);
+  animatedObject.update();
 }
 function moveup() {
-  redSquare.y -= 30;
+ 
+  animatedObject.y -= 30;
 }
 
 function movedown() {
-  redSquare.y += 30;
+  
+  animatedObject.y += 30;
 }
 
 function moveleft() {
-  redSquare.x -= 30;
+  
+  animatedObject.x -= 30;
 }
 
 function moveright() {
-  redSquare.x += 30;
+ 
+  animatedObject.x += 30;
 }
 function clearmove() {
-    redSquare.speedX = 0; 
-    redSquare.speedY = 0; 
+    animatedObject.speedX = 0; 
+    animatedObject.speedY = 0; 
 }
+
+
+
+var animatedObject = {
+  speedX: 0,
+  speedY: 0,
+  width: 60,
+  height: 60,
+  x: 10,
+  y: 120,
+  imageList: [], 
+  contaFrame: 0, 
+  actualFrame: 0, 
+
+  update: function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.contaFrame++;
+    if (this.contaFrame == 50) {
+      this.contaFrame = 0;
+      this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+        this.image = this.imageList[this.actualFrame];
+    }
+  },
+
+  idle: function(){
+     this.image = new Image(this.width, this.height);
+    this.image.src = "https://i.ibb.co/4wG3nPHb/Idle-000.png";
+  },
+  
+
+  loadImages: function() {
+     for (imgPath of running) {
+      var img = new Image(this.width, this.height);
+      img.src = imgPath;
+      this.imageList.push(img);
+     
+    }
+    this.image = this.imageList[this.actualFrame];
+  }
+};
+
+
+
 
---
+++ sprite.js
@@ -0,0 +1,12 @@
+var running=[
+ "https://i.ibb.co/nMHM6P0Z/Run-000.png",
+ "https://i.ibb.co/ksy23gQ0/Run-001.png",
+ "https://i.ibb.co/yn098Zc3/Run-002.png",
+ "https://i.ibb.co/BVR1r6vC/Run-003.png",
+ "https://i.ibb.co/8Dhqryzj/Run-004.png",
+ "https://i.ibb.co/1tzBQsX9/Run-005.png",
+ "https://i.ibb.co/MkcZrqSY/Run-006.png",
+ "https://i.ibb.co/cStcFMLk/Run-007.png",
+ "https://i.ibb.co/zWhGyYWx/Run-008.png",
+ "https://i.ibb.co/WpjSGbm4/Run-009.png"
+]
